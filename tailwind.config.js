1b6c076d72f82a0440c90269beeb79989941b970
Odiljon2008 <odiljon7240@gmail.com>
2024-05-01 05:08:11 -0700

    nev

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        inter:['HelveticaNeue']
+      },
+      colors:{
+        brend:{
+          gray:{
+            100:"#1C1A28",
+            200:"#306EE8",
+            300:"#D2D3D6",
+            400:"#232031",
+            500:"#232031",
+            600:"#0F1B33",
+            700:"#112857",
+            800:"rgba(255, 255, 255, 0.08)",
+            900:"#416ABD"
+
+          }
+        }
+      },
+    },
   },
   plugins: [],
 }
